b1e19ec9fbbef061949c348ed1a44b913416d9bb
sakkkurai <sakurai.pnwz@gmail.com>
2025-07-11 13:18:25 +0300

    added parsing artists in fragment

--- app/src/main/java/com/sakkkurai/venok/ui/fragments/ArtistFragment.java
+++ app/src/main/java/com/sakkkurai/venok/ui/fragments/ArtistFragment.java
@@ -1,24 +1,33 @@
 package com.sakkkurai.venok.ui.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sakkkurai.venok.R;
 import com.sakkkurai.venok.adapters.ArtistAdapter;
+import com.sakkkurai.venok.models.Track;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ArtistFragment extends Fragment {
     private RecyclerView artistLibrary;
+    private String TAG = "ArtistFragment";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,17 +36,39 @@ public class ArtistFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_artist, container, false);
 
         artistLibrary = v.findViewById(R.id.artist_artistLibrary);
-        createArtistList();
         artistLibrary.setLayoutManager(new LinearLayoutManager(getContext()));
+        setArtistLibrary();
         return v;
     }
 
-    private void createArtistList() {
-        List<String> list = new ArrayList<>();
-        list.add("sex");
-        list.add("hitler");
-        list.add("eblan");
-        ArtistAdapter artistAdapter = new ArtistAdapter(getActivity(), new ArrayList<>(list));
+
+    private void setArtistLibrary() {
+        List<String> artists = new ArrayList<>();
+        String sortOrder = MediaStore.Audio.Media.ARTIST + " ASC";
+
+        String[] projection = {
+                MediaStore.Audio.Media.ARTIST,
+        };
+
+        Uri audioUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        try (Cursor cursor = contentResolver.query(audioUri, projection, null, null, sortOrder)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    String artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
+                    artists.add(artist);
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.e("Library", "Error loading artists: " + e.getMessage());
+        }
+        Set<String> set = new HashSet<>(artists);
+        artists.clear();
+        artists.addAll(set);
+        ArtistAdapter artistAdapter = new ArtistAdapter(getActivity(), new ArrayList<>(artists));
         artistLibrary.setAdapter(artistAdapter);
+        Log.d(TAG, String.valueOf(artists));
+
+
     }
 }
